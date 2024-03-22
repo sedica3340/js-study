037e97978568c531a7a5c3ed8c79222bc94f7ccc
sedica3340 <sedica3340@gmail.com>
2024-03-22 09:50:05 +0900

    asd

---
+++ core-javascript/02제어문/16continue.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     console.log(i + '날아가는 새들 바라보며 나도함께 날아가고 싶어 파란하늘 아래서 자유롭게');
+// }
+
+while (true) {
+    let n = +prompt('숫자');
+
+    if (n === 0) break;
+    else if (n === 1) continue;
+    alert('날아가는 새들 바라보며');
+}
+alert('r');
