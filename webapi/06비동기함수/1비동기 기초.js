46adf55009bf565b5e5afa81ea5fb8a6c781f46e
sedica3340 <sedica3340@gmail.com>
2024-04-02 15:35:35 +0900

    asd

---
+++ webapi/06비동기함수/1비동기 기초.js
@@ -0,0 +1,18 @@
+// 비동기함수 setTimeout
+// 코드를 순서대로 진행시키는게 아닌 시간차로 진행시킬 수 있게 함
+
+console.log("오늘 할 일은 뭘까요?");
+
+setTimeout(() => {
+    console.log("오늘은 집에서 쉴거예요!");
+}, 2000);
+setTimeout(() => {
+    for (let i = 0; i < 3; i++) {
+        console.log(`hello${i}`);
+    }
+}, 2000);
+setTimeout(() => {
+    for (let i = 0; i < 4; i++) {
+        console.log(`bye${i}`);
+    }
+}, 0);
