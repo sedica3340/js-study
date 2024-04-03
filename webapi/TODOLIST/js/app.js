570f51722719ed0d3eca473d3b55f9efad1d1445
sedica3340 <sedica3340@gmail.com>
2024-04-03 10:53:08 +0900

    1차 기능완성

--- webapi/TODOLIST/js/app.js
+++ webapi/TODOLIST/js/app.js
@@ -1,3 +1,22 @@
+//==============서버 통신 데이터=====//
+const todos = [
+    {
+        id: 1,
+        text: "할 일 1",
+        done: false,
+    },
+    {
+        id: 2,
+        text: "할 일 2",
+        done: false,
+    },
+    {
+        id: 3,
+        text: "할 일 3",
+        done: false,
+    },
+];
+
 //=======================함수 선언================//
 let listCount = 3;
 function addPlan() {
@@ -7,16 +26,22 @@ function addPlan() {
     const $newLi = document.createElement("li");
     $newLi.dataset.id = `${listCount}`;
     $newLi.classList.add("todo-list-item");
-    $newLi.innerHTML = `<label class="checkbox">
-    <input type="checkbox">
-    <span class="text">${value}</span>
-</label>
-<div class="modify">
-    <span class="lnr lnr-undo"></span>
-</div>
-<div class="remove">
-    <span class="lnr lnr-cross-circle"></span>
-</div>`;
+    $newLi.innerHTML = `
+    <label class="checkbox">
+        <input type="checkbox">
+            <span class="text">${value}</span>
+    </label>
+    <div class="modify">
+        <span class="lnr lnr-undo"></span>
+    </div>
+    <div class="remove">
+        <span class="lnr lnr-cross-circle"></span>
+    </div>`;
+    const todo = {};
+    todo.id = listCount;
+    todo.text = value;
+    todo.done = false;
+    todos.push(todo);
     document.querySelector(".todo-list").appendChild($newLi);
     document.querySelector("#todo-text").value = "";
 }
@@ -32,11 +57,56 @@ document.querySelector("#todo-text").addEventListener("keyup", (e) => {
 });
 document.querySelector(".todo-template").addEventListener("click", (e) => {
     if (!e.target.matches("div.remove span")) return;
-    document.querySelector('.todo-list').removeChild(e.target.parentElement.parentElement);
+    document
+        .querySelector(".todo-list")
+        .removeChild(e.target.parentElement.parentElement);
+    const index = todos.findIndex(
+        (todo) => todo.id === +e.target.parentElement.parentElement.dataset.id
+    );
+    if (index !== -1) {
+        todos.splice(index, 1);
+    }
 });
-document.querySelector(".todo-template").addEventListener('click', e => {
-    console.log(e.target.parentElement.parentElement.firstElementChild.lastElementChild.textContent);
+document.querySelector(".todo-template").addEventListener("click", (e) => {
+    // console.log(e.target.parentElement.parentElement.firstElementChild);
+    const $label = e.target.parentElement.parentElement.firstElementChild;
+    // const $input = e.target.parentElement.parentElement.firstElementChild.firstElementChild;
+    // let newValue = "";
     if (!e.target.matches("div.modify span")) return;
-    e.target.parentElement.parentElement.firstElementChild.lastElementChild.textContent = 
-    
-})
+    if (e.target.classList.contains("lnr-undo")) {
+        const $span =
+            e.target.parentElement.parentElement.firstElementChild
+                .lastElementChild;
+        const value = $span.textContent;
+        const $newInput = document.createElement("input");
+        $newInput.style.border = "1px solid gray";
+        $newInput.style.borderRadius = "6px";
+        $newInput.style.marginLeft = "60px";
+        $newInput.value = value;
+        $newInput.classList.add("modi");
+        // $input.style.display = 'none';
+        $label.replaceChild($newInput, $span);
+        e.target.classList.remove("lnr-undo");
+        e.target.classList.add("lnr-checkmark-circle");
+    } else if (e.target.classList.contains("lnr-checkmark-circle")) {
+        const $newInput = document.querySelector(".modi");
+        const value = $newInput.value;
+        if (value.length === 0) return;
+        const $newSpan = document.createElement("span");
+        $newSpan.classList.add("text");
+        $newSpan.textContent = value;
+        $label.replaceChild($newSpan, $newInput);
+        e.target.classList.remove("lnr-checkmark-circle");
+        e.target.classList.add("lnr-undo");
+        // $label.replaceChild($span, $newInput);
+    }
+});
+document.querySelector(".todo-template").addEventListener("change", (e) => {
+    if (!e.target.matches("label.checkbox input")) return;
+    // console.log(+e.target.parentElement.parentElement.dataset.id);
+    const index = todos.findIndex(
+        (todo) => todo.id === +e.target.parentElement.parentElement.dataset.id
+    );
+    todos[index].done = !todos[index].done;
+    // console.log(index);
+});
